73a5cabd0fe6cd56f340933369e1893198112db2
Daniel Santos <dsantosp12@gmail.com>
2018-11-21 20:19:12 -0500

    Added stack to run C code

---
+++ kmain.c
@@ -0,0 +1,5 @@
+
+int kmain()
+{
+  return 9;
+}
--- loader.s
+++ loader.s
@@ -1,18 +1,28 @@
-global loader                   ; the entry symbol for ELF
+global loader                               ; the entry symbol for ELF
 
-MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-FLAGS        equ 0x0            ; multiboot flags
-CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                ; magic number + checksum + flags should equal 0
+MAGIC_NUMBER equ 0x1BADB002                 ; define the magic number constant
+FLAGS        equ 0x0                        ; multiboot flags
+CHECKSUM     equ -MAGIC_NUMBER              ; calculate the checksum
+                                            ; (magic number + checksum + flags should equal 0)
 
-section .text                   ; start of the text (code) section
-align 4                         ; the code must be 4 byte aligned
-  dd MAGIC_NUMBER               ; dd write to machine code
-  dd FLAGS
-  dd CHECKSUM
+section .text:                              ; start of the text (code) section
+align 4                                     ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER                         ; write the magic number to the machine code,
+    dd FLAGS                                ; the flags,
+    dd CHECKSUM                             ; and the checksum
 
-loader:                         ; the loader label (defined as entry point in linker script)
-  mov eax, 0xCAFEBABE           ; place the number 0xCAFEBABE in the register eax
-
+loader:                                     ; the loader label (defined as entry point in linker script)
+    mov eax, 0xCAFEBABE                     ; place the number 0xCAFEBABE in the register eax
 .loop:
-  jmp .loop                     ; loop forever
+    jmp .loop                               ; loop forever
+
+KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
+
+section .bss
+align 4                                     ; align at 4 bytes
+kernel_stack:                               ; label points to beginning of memory
+  resb KERNEL_STACK_SIZE                    ; reserve stack for the kernel
+  mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the stack (end of memory area)
+
+extern kmain
+call kmain
